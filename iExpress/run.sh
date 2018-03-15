847de68dcd68ee9c1caacfd5f3147dcf1e7ae2fa
sull <sulele@supermap.com>
2018-03-15 13:38:51 +0800

    get iexpress.tar.gz from http

--- iExpress/run.sh
+++ iExpress/run.sh
@@ -1,8 +1,8 @@
 # 重装许可驱动，获取使用许可（正式购买许可后不需要这一步）
-cd /opt/SuperMapiExpress7C/support/SuperMap_License/Support/aksusbd-2.4.1-i386
+cd /opt/SuperMapiExpress/support/SuperMap_License/Support/aksusbd-2.4.1-i386
 ./dunst
 ./dinst
 echo
 # 运行iExpress
-cd /opt/SuperMapiExpress7C/bin
+cd /opt/SuperMapiExpress/bin
 ./catalina.sh run
